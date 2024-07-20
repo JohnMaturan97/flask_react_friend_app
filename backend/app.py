d71041ca6b4e9f959bb17652bd52a2b68da29fcf
JohnMaturan97 <john.maturan.-nd@disney.com>
2024-07-20 19:29:41 -0400

    project setup

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+# TODO: UPDATE THIS FILE FOR DEPLOYMENT
+from flask import Flask, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://psqldba:g5fr0nich6CaBo9h@apsql-apm1234567-prdcl01.cti2yaq020bk.us-east-1.rds.amazonaws.com:5432/friends"
+db = SQLAlchemy(app)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# api routes
+import routes
+
+with app.app_context():
+  db.create_all()
+
+if __name__ == "__main__":
+  app.run(debug=True)
