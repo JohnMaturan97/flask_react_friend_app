944387c18129832b513076306d7c57c4b909edbf
JohnMaturan97 <borntocode1997@gmail.com>
2024-07-21 06:28:37 -0400

    frontend code setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Button, Container, Stack } from "@chakra-ui/react"
+import Narbar from "./components/Narbar"
+
+function App() {
+  return (
+    <Stack minH={"100vh"}>
+      <Narbar/>
+
+      <Container maxW={"1200px"} my={4}>
+
+      </Container>
+    </Stack>
+  )
+}
+
+export default App
