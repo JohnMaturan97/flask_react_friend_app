56fc50bc109c071c51e8e706c7400d78d0393ea5
JohnMaturan97 <borntocode1997@gmail.com>
2024-07-20 21:09:25 -0400

    backend code

--- backend/app.py
+++ backend/app.py
@@ -7,16 +7,19 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://psqldba:g5fr0nich6CaBo9h@apsql-apm1234567-prdcl01.cti2yaq020bk.us-east-1.rds.amazonaws.com:5432/friends"
-db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql://psqldba:g5fr0nich6CaBo9h@apsql-apm1234567-prdcl01.cti2yaq020bk.us-east-1.rds.amazonaws.com:5432/friends"
+)
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+
 # api routes
-import routes
+from routes import *
 
 with app.app_context():
-  db.create_all()
+    db.create_all()
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
